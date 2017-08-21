dadb180e8fafc6b60041e6cc7478e30ddf5b5e06
Nathan Fenner <nathanfenner@Nathans-MacBook-Pro-2.local>
2017-08-21 16:53:17 -0700

    Added comments to media.py

--- media.py
+++ media.py
@@ -2,11 +2,15 @@ import webbrowser
 
 
 class Movie():
+    """A model of a Movie"""
+
     def __init__(self, movie_title, storyline, poster_image, trailer_youtube):
+        """Initialize title, storyline, poster_image, and trailer attributes"""
         self.title = movie_title
         self.storyline = storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """Open webbrowser at specified trailer URL"""
         webbrowser.open(self.trailer_youtube_url)
